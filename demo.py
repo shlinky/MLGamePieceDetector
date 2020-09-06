730d1210d5934cfb8aa241ca0b7d2523dd3a14bd
shlok shah <2004shlok@gmail.com>
2020-09-06 12:10:22 -0700

    small changes

--- demo.py
+++ demo.py
@@ -2,9 +2,9 @@ from Classification.Training.Model_Builder import ModelBuilder
 from Localization.Localizer import Localizer
 from World_Location_Detection.LocationCalculation import LocationCalculator
 
-image_dir = 'C:/Users/shloks/Documents/robproj/MLGamePieceDetector/Classification/Data/extra_imgs/1data39.png'
-class_model = ModelBuilder(ckpt = 'C:/Users/shloks/Documents/robproj/MLGamePieceDetector/Classification/Training/checkpoints/LL_NEWSA').get_model()
-l = Localizer(class_model, detection_weights = 'C:/Users/shloks/Documents/robproj/MLGamePieceDetector/Localization/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8/saved_model')
+image_dir = 'Classification/Data/extra_imgs/1data39.png'
+class_model = ModelBuilder(ckpt = 'Classification/Training/checkpoints/LL_NEWSA').get_model()
+l = Localizer(class_model, detection_weights = 'Localization/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8/saved_model')
 calc = LocationCalculator(camera_const = 1.6, FOV = 120, obj_size = 7)
 
 results = l.call(image_dir, display = True)
